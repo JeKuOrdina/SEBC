63fe41c0f8384cadbbd1b899395236cc52e31aa5
JeKuOrdina <Jen-Yin.Keung@ordina.be>
2018-03-13 16:39:20 +0100

    Resource Lab

---
+++ resources/labs/4_YARN_results.md
@@ -0,0 +1,29 @@
+[ec2-user@cdh-agent-0 ~]$ ./yarn.sh
+```
+
+    Testing loop started on Tue Mar 13 15:29:16 UTC 2018
+    8 8 512
+
+    real    0m33.125s
+    user    0m5.430s
+    sys     0m0.245s
+
+    real    1m36.801s
+    user    0m7.814s
+    sys     0m0.359s
+
+```
+
+```
+
+        8 8 1024
+
+        real    0m33.186s
+        user    0m5.360s
+        sys     0m0.260s
+
+        real    1m33.087s
+        user    0m6.828s
+        sys     0m0.329s
+
+```
--- resources/tools/YARNtest.sh
+++ resources/tools/YARNtest.sh
@@ -11,31 +11,32 @@ echo Testing loop started on `date`
 for i in 8    
 do
    # Reducer containers
-   for j in 1 
+   for j in 4 
    do                 
       # Container memory
       for k in 512 1024 
-      do                         
+      do                       
+         echo ${i}_${j}_${k}  
          # Set mapper JVM heap 
          MAP_MB=`echo "($k*0.8)/1" | bc` 
 
          # Set reducer JVM heap 
          RED_MB=`echo "($k*0.8)/1" | bc` 
 
-        time ${HADOOP}/hadoop jar ${MR}/hadoop-examples.jar teragen \
+         time ${HADOOP}/hadoop jar ${MR}/hadoop-examples.jar teragen \
                      -Dmapreduce.job.maps=$i \
                      -Dmapreduce.map.memory.mb=$k \
                      -Dmapreduce.map.java.opts.max.heap=$MAP_MB \
                      51200000 /results/tg-10GB-${i}-${j}-${k} 1>tera_${i}_${j}_${k}.out 2>tera_${i}_${j}_${k}.err                       
 
-       time ${HADOOP}/hadoop jar $MR/hadoop-examples.jar terasort \
+         time ${HADOOP}/hadoop jar $MR/hadoop-examples.jar terasort \
                      -Dmapreduce.job.maps=$i \
                      -Dmapreduce.job.reduces=$j \
                      -Dmapreduce.map.memory.mb=$k \
                      -Dmapreduce.map.java.opts.max.heap=$MAP_MB \
                      -Dmapreduce.reduce.memory.mb=$k \
                      -Dmapreduce.reduce.java.opts.max.heap=$RED_MB \
-	             /results/tg-10GB-${i}-${j}-${k}  \
+	                   /results/tg-10GB-${i}-${j}-${k}  \
                      /results/ts-10GB-${i}-${j}-${k} 1>>tera_${i}_${j}_${k}.out 2>>tera_${i}_${j}_${k}.err                         
 
         $HADOOP/hadoop fs -rm -r -skipTrash /results/tg-10GB-${i}-${j}-${k}                         
